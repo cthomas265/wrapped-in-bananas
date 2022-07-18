0bf82ee9086d232c8826a6113e911d645a2f9846
Aubree Alexander <aubree.alexander@gmail.com>
2022-07-18 18:44:59 -0500

    create react app, install dependencies

---
+++ server/utils/auth.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');  
+const secret = 'mysecretsshhhhh'; 
+const expiration = '2h';  
+
+module.exports = {   
+    signToken: function({ username, email, _id }) 
+    {     
+        const payload = { username, email, _id };      
+        return jwt.sign({ data: payload }, 
+            secret, 
+            { expiresIn: expiration });  
+    }
+
+};
