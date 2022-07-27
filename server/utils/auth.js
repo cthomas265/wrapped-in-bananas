3677e889281ef78c82b3da97187a590b4f0d791b
Carolyn Hudson <96785153+cghudson@users.noreply.github.com>
2022-07-26 19:45:48 -0500

    fix auth bug

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,33 +1,32 @@
-const jwt = require("jsonwebtoken");
-const { AuthenticationError } = require("apollo-server-express");
-const secret = "mysecretsshhhhh";
-const expiration = "2h";
+const jwt = require('jsonwebtoken')
+const { AuthenticationError } = require('apollo-server-express')
+const secret = 'mysecretsshhhhh'
+const expiration = '2h'
 
 module.exports = {
-  authMiddleware: function ({ req }) {
-    let token = req.body.token || req.query.token || req.headers.authorization;
+  authMiddleware: function({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization
     if (req.headers.authorization) {
-      token = token.split(" ").pop().trim();
+      token = token.split(' ').pop().trim()
     }
     if (!token) {
-      return req;
+      return req
     }
     try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch (err) {
-      console.log("Invalid token");
+      const { data } = jwt.verify(token, secret, { maxAge: expiration })
+      req.user = data
+    } catch(err) {
+      console.log('Invalid token')
     }
+    return res
   },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  signToken: function({ _id, email, username }) {
+    const payload = { _id, email, username }
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration })
   },
-  checkAuth: function (context) {
-    if (process.env.NODE_ENV === "production" && !context.user) {
-      throw new AuthenticationError(
-        "You must be logged in to query this schema!"
-      );
+  checkAuth: function(context) {
+    if (process.env.NODE_ENV === 'production' && !context.user) {
+      throw new AuthenticationError('You must be logged in to query this schema.')
     }
-  },
-};
+  }
+}
