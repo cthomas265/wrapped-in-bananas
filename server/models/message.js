f7575f2bc57822dffcee64e4c74df4357577af98
Carolyn Hudson <96785153+cghudson@users.noreply.github.com>
2022-07-31 12:42:15 -0500

    create message and comment models, add date format helper

--- server/models/message.js
+++ server/models/message.js
@@ -1,4 +1,6 @@
 const { Schema, model, Types } = require('mongoose')
+const commentSchema = require ('./comment')
+const dateFormat = require('../utils/dateFormat')
 
 //AA - how to incorporate signatures?
 //AA - need this model to reference the user model who posts the message
@@ -8,19 +10,29 @@ const messageSchema = new Schema(
       messageBody: {
         type: String,
         required: "A message is required.",
+        minlength: 1,
+        maxlentgh: 280
       },
-      messageId: {
-        type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId()
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: timestamp => dateFormat(timestamp)
       },
+      // messageId: {
+      //   type: Schema.Types.ObjectId,
+      //       default: () => new Types.ObjectId()
+      // },
       //AA - will eventually want this to reference an existing user id
       username: {
         type: String,
         required: true
-      }
+      },
+      comments: [commentSchema]
     },
     {
-      timestamps: true,
+      toJSON: {
+        getters: true
+      },
     }
 );
 
