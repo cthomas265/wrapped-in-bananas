b305a7c2c02f8c499563e4311b942588d7ba9295
woodb58 <woodb58@gmail.com>
2022-08-02 13:11:40 -0500

    added pics to staudentData

--- client/src/pages/studentData.js
+++ client/src/pages/studentData.js
@@ -1,34 +1,118 @@
-import pic from "../assets/img/test.png";
+import alan from "./images/studentPhotos/alan-e.png"
+import alexC from "./images/studentPhotos/alex-c.png"
+import alexR from "./images/studentPhotos/alex-r.png"
+import aubree from "./images/studentPhotos/aubree-a.png"
+import cameron from "./images/studentPhotos/cameron-s.png"
+import carolyn from "./images/studentPhotos/carolyn-h.png"
+import carrie from "./images/studentPhotos/carrie-t.JPG"
+import david from "./images/studentPhotos/david-w.png"
+import emily from "./images/studentPhotos/emily-b.jpg"
+import jake from "./images/studentPhotos/jake-t.png"
+import james from "./images/studentPhotos/james-a.png"
+import joshN from "./images/studentPhotos/josh-n.png"
+import joshR from "./images/studentPhotos/josh-r.png"
+import kristina from "./images/studentPhotos/kristina-b.png"
+import mikeM from "./images/studentPhotos/mike-m.png"
+import mikeS from "./images/studentPhotos/mike-s.png"
+import pat from "./images/studentPhotos/patrick-a.png"
+import robby from "./images/studentPhotos/robby-k.png"
+import scott from "./images/studentPhotos/scott-m.png"
+
 
 const StudentData = [
   {
-    img: pic,
-    name: "test1",
+    img: alan,
+    name: "alan",
     bio: "this is a bio",
   },
   {
-    img: pic,
-    name: "test2",
+    img: alexC,
+    name: "alexC",
     bio: "this is a bio",
   },
   {
-    img: pic,
-    name: "test3",
+    img: alexR,
+    name: "alexR",
     bio: "this is a bio",
   },
   {
-    img: pic,
-    name: "test4",
+    img: aubree,
+    name: "aubree",
     bio: "this is a bio",
   },
   {
-    img: pic,
-    name: "test5",
+    img: cameron,
+    name: "cameron",
     bio: "this is a bio",
   },
   {
-    img: pic,
-    name: "test6",
+    img: carolyn,
+    name: "carolyn",
+    bio: "this is a bio",
+  },
+  {
+    img: carrie,
+    name: "carrie",
+    bio: "this is a bio",
+  },
+  {
+    img: david,
+    name: "david",
+    bio: "this is a bio",
+  },
+  {
+    img: emily,
+    name: "emily",
+    bio: "this is a bio",
+  },
+  {
+    img: jake,
+    name: "jake",
+    bio: "this is a bio",
+  },
+  {
+    img: james,
+    name: "james",
+    bio: "this is a bio",
+  },
+  {
+    img: joshN,
+    name: "joshN",
+    bio: "this is a bio",
+  },
+  {
+    img: joshR,
+    name: "joshR",
+    bio: "this is a bio",
+  },
+  {
+    img: kristina,
+    name: "kristina",
+    bio: "this is a bio",
+  },
+  {
+    img: mikeM,
+    name: "mikeM",
+    bio: "this is a bio",
+  },
+  {
+    img: mikeS,
+    name: "mikeS",
+    bio: "this is a bio",
+  },
+  {
+    img: pat,
+    name: "pat",
+    bio: "this is a bio",
+  },
+  {
+    img: robby,
+    name: "robby",
+    bio: "this is a bio",
+  },
+  {
+    img: scott,
+    name: "scott",
     bio: "this is a bio",
   }
 ];
