daa3e3475a0b53f992e3c68b3ca65f02822801a1
woodb58 <woodb58@gmail.com>
2022-08-03 16:40:13 -0500

    added insert pages for collages and added signatures

--- client/src/pages/studentPage.js
+++ client/src/pages/studentPage.js
@@ -5,17 +5,33 @@ import StudentData from "./studentData";
 import StudentCard from "./studentCard";
 import FrontCover from "./frontCover";
 import BackCover from "./backCover";
-import PaddingPage from "./paddingPage";
+import ClassCollage from "./classCollage"
+import PetCollage from "./petCollage"
+import Signature from "./signature"
 
 const PageCover = React.forwardRef((props, ref) => {
   return (
     <div className="page page-cover" ref={ref} data-density="hard">
-      <div className="page-content">
-        {props.children}
-        </div>
+      <div className="page-content">{props.children}</div>
     </div>
   );
 });
+
+const InsertPage = React.forwardRef((props, ref) => {
+  return (
+    <div className="page" ref={ref}>
+      <div className="page-content">{props.children}</div>
+    </div>
+  );
+});
+
+// const BackPage = React.forwardRef((props, ref) => {
+//   return (
+//     <div className="page" ref={ref}>
+//       <div className="page-content">{props.children}</div>
+//     </div>
+//   );
+// });
 
 const Page = React.forwardRef((props, ref) => {
   return (
@@ -32,12 +48,12 @@ const Page = React.forwardRef((props, ref) => {
 function StudentPage(props) {
   return (
     <HTMLFlipBook width={900} height={850} className="demo-block600">
-      <PageCover >
-        <PaddingPage />
-      </PageCover>
       <PageCover>
         <FrontCover />
       </PageCover>
+      <InsertPage>
+        <ClassCollage />
+      </InsertPage>
       {StudentData.map((val, i) => {
         return (
           <Page number={i + 1} key={i} className="page">
@@ -45,11 +61,14 @@ function StudentPage(props) {
           </Page>
         );
       })}
+      <InsertPage>
+        <Signature />
+      </InsertPage>
+      <InsertPage>
+        <PetCollage />
+      </InsertPage>
       <PageCover>
         <BackCover />
-      </PageCover>
-      <PageCover>
-        <PaddingPage />
       </PageCover>
     </HTMLFlipBook>
   );
